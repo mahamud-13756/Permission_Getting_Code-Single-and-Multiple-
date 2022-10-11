678272c34f63f5db5ddd6d48d30a60de6aa15feb
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-10-11 20:15:52 +0600

    connect the button with java file

--- app/src/main/java/com/example/flash_light_app/MainActivity.java
+++ app/src/main/java/com/example/flash_light_app/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.flash_light_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton b_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_image = findViewById(R.id.btn_torch);
     }
 }
